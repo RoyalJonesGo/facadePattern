5292f4c502563e92dced29376b1ae5eb6123438d
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-09-27 10:58:29 +0800

    Create HomeApp.java

---
+++ HomeApp.java
@@ -0,0 +1,13 @@
+import facade.HomeInterface;
+
+public class HomeApp {
+    public static void main(String[] args) {
+        HomeInterface homeInterface = new HomeInterface();
+
+        System.out.println("\nServices turning ON:");
+        homeInterface.turnOnAll();
+
+        System.out.println("\nServices turning OFF:");
+        homeInterface.turnOffAll();
+    }
+}
