be0dad282d07a10960555bffcf23fef04a59b7c7
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-09-27 10:58:01 +0800

    Create TV.java

---
+++ services/TV.java
@@ -0,0 +1,13 @@
+package services;
+
+public class TV implements HomeService {
+    @Override
+    public void turnOn() {
+        System.out.println("TV is on.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV is off.");
+    }
+}
